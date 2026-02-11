2f722848f69b62e432376e85fa65e5f781533f28
1111475 <2012253499@qq.com>
2026-02-11 11:47:59 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -1,18 +1,8 @@
 #!/bin/bash
-#
-# https://github.com/P3TERX/Actions-OpenWrt
-# File name: diy-part1.sh
-# Description: OpenWrt DIY script part 1 (Before Update feeds)
-#
-# Copyright (c) 2019-2024 P3TERX <https://p3terx.com>
-#
-# This is free software, licensed under the MIT License.
-# See /LICENSE for more information.
-#
 
-# Uncomment a feed source
-#sed -i 's/^#\(.*helloworld\)/\1/' feeds.conf.default
+# 1. 添加科學上網插件源 (small-package 是目前最穩定的精簡源)
+echo 'src-git small8 https://github.com/kenzok8/small-package' >> feeds.conf.default
 
-# Add a feed source
-echo 'src-git helloworld https://github.com/fw876/helloworld' >>feeds.conf.default
-#echo 'src-git passwall https://github.com/xiaorouji/openwrt-passwall' >>feeds.conf.default
+# 2. 移除重複包 (防止編譯衝突)
+rm -rf package/lean/luci-app-ssr-plus
+rm -rf package/lean/luci-app-bypass
